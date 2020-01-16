3e8a3d7239bc707c758d152a78f4a257e23bfcee
Gonzalo <grzuy0@gmail.com>
2020-01-16 16:30:22 -0300

    feat: provide TPM::KeyAttestation#valid?

--- lib/tpm/key_attestation.rb
+++ lib/tpm/key_attestation.rb
@@ -3,7 +3,37 @@
 require "tpm/key_attestation/version"
 
 module TPM
-  module KeyAttestation
+  class KeyAttestation
     class Error < StandardError; end
+
+    attr_reader :certify_info, :signature, :certified_object, :signing_key, :hash_function, :qualifying_data
+
+    def initialize(certify_info, signature, certified_object, signing_key, hash_function, qualifying_data)
+      @certify_info = certify_info
+      @signature = signature
+
+      @certified_object = certified_object
+      @signing_key = signing_key
+      @hash_function = hash_function
+      @qualifying_data = qualifying_data
+    end
+
+    def valid?
+      valid_signature? && valid_certify_info?
+    end
+
+    private
+
+    def valid_signature?
+      signing_key.verify(hash_function, signature, certify_info)
+    end
+
+    def valid_certify_info?
+      s_attest.valid?(certified_object, qualifying_data)
+    end
+
+    def s_attest
+      @s_attest ||= ::TPM::SAttest.read(certify_info)
+    end
   end
 end
--- lib/tpm/key_attestation/version.rb
+++ lib/tpm/key_attestation/version.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 module TPM
-  module KeyAttestation
+  class KeyAttestation
     VERSION = "0.1.0"
   end
 end

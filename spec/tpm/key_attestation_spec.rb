411ab2bc4cd7aa89b3cf988a205a653624c1d71c
Gonzalo <grzuy0@gmail.com>
2020-01-16 11:10:27 -0300

    test: remove dummy test case

--- spec/tpm/key_attestation_spec.rb
+++ spec/tpm/key_attestation_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe TPM::KeyAttestation do
   it "has a version number" do
     expect(TPM::KeyAttestation::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
